16fe951a22ac3196a795402e56b3afa1ef554e36
FacundoYoris <yorisfacundo@gmail.com>
2023-11-30 19:32:26 -0300

    Observar ordenes OK(Pendientes- en proceso - finalizadas)

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,10 +21,47 @@ const router = Router()
 router.get('/', (req, res) => res.render('inicio_sesion.ejs', {"x": false })) 
 //Cuando el servidor este en la pagina inicial ejecutar inicio_sesion.ejs
 router.get('/estadistica', (req, res) => res.render('estadistica.ejs', {"login": req.session.loggedImAdmin}))
-router.get('/orden-de-trabajo', (req, res) => res.render('orden_trabajo.ejs', {"login": req.session.loggedImAdmin}))
-router.get('/orden-de-trabajo/pendiente', (req, res) => res.render('pendiente.ejs', {"login": req.session.loggedImAdmin}))
-router.get('/orden-de-trabajo/en-proceso', (req, res) => res.render('en_proceso.ejs', {"login": req.session.loggedImAdmin}))
-router.get('/orden-de-trabajo/finalizada', (req, res) => res.render('finalizada.ejs', {"login": req.session.loggedImAdmin}))
+router.get('/orden-de-trabajo', (req, res) => { 
+    connection.query('SELECT * FROM orden_trabajo', (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.render('orden_trabajo.ejs', {results: results,"login": req.session.loggedImAdmin});
+        }
+
+    })
+})
+
+router.get('/orden-de-trabajo/pendiente', (req, res) => { 
+    connection.query('SELECT * FROM orden_trabajo WHERE estado = ?', ['Pendiente'] , (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.render('pendiente.ejs', {results: results,"login": req.session.loggedImAdmin});
+        }
+
+    })
+})
+router.get('/orden-de-trabajo/finalizada', (req, res) => { 
+    connection.query('SELECT * FROM orden_trabajo WHERE estado = ?', ['Finalizada'] , (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.render('finalizada.ejs', {results: results,"login": req.session.loggedImAdmin});
+        }
+
+    })
+})
+router.get('/orden-de-trabajo/en-proceso', (req, res) => { 
+    connection.query('SELECT * FROM orden_trabajo WHERE estado = ?', ['En proceso'] , (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.render('en_proceso.ejs', {results: results,"login": req.session.loggedImAdmin});
+        }
+
+    })
+})
 router.get('/orden-de-trabajo/generar-orden', (req, res) => { 
     connection.query('SELECT * FROM orden_trabajo', (error, results)=>{
         if(error){
@@ -50,11 +87,23 @@ router.get('/orden-de-trabajo/edit/:id', (req, res) => {
 
         });
 });
+//RUTA PARA ELIMINAR UNA ORDEN
+router.get('/orden-de-trabajo/delete/:id', (req, res) => {
+    const id = req.params.id;
+    connection.query('DELETE FROM orden_trabajo WHERE id_orden_trabajo = ?', [id], (error, results)=>{
+        
+        if(error){
+            throw error;
+        }else{
+            res.redirect('/orden-de-trabajo/generar-orden');
+        }
+
+        });
+});
 router.get('/almacen', (req, res) => res.render('almacen.ejs', {"login": req.session.loggedImAdmin}))
 router.get('/gestion-mantenimiento', (req, res) => res.render('gestion_mantenimiento.ejs', {"login": req.session.loggedImAdmin}))
 router.get('/terceros', (req, res) => res.render('terceros.ejs', {"login": req.session.loggedImAdmin}))
 router.get('/administrar-usuario', (req, res) => res.render('administrar_usuario.ejs', {"login": req.session.loggedImAdmin}))
-
 import save from '../controllers/gestion_orden_trabajo.js';
 router.post('/save', save.save);
 router.post('/update', save.update);
