4daf65fe7a381c9f7ee9d38eaef14cb29f885fc4
FacundoYoris <yorisfacundo@gmail.com>
2024-03-12 11:32:14 -0300

    Cambios en configuracion de la BDD

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,9 @@
 
 import mysql2 from 'mysql2';
 const connection = mysql2.createPool({
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	database: 'innway_ingenieria'
+	host: 'biughjwhbdg2q0yrwblg-mysql.services.clever-cloud.com',
+	user: 'umzfvpwgo5uu9npw',
+	password: 'BEcIMMtlk17QUIstuw9p',
+	database: 'biughjwhbdg2q0yrwblg'
 });
 export default connection;
